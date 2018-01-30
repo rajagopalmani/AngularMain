bde37993bf725c44f1bb1cf786b86894b967b3f1
Raja Gopal Mani <rmani@opentext.com>
2018-01-31 01:21:59 +0530

    'commit'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { Routes, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      [
+        { path: 'external', loadChildren: '@angular-poc/module1/release/dist#AppModule1' } 
+      ], { useHash: true, enableTracing: false }
+    ),
     BrowserModule
   ],
   providers: [],
